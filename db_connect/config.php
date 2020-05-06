10dc25ddc5f6b8774b7dce66f05a87c337667258
axelehrlich59 <53488067+axelehrlich59@users.noreply.github.com>
2020-05-06 18:31:03 +0200

    done with form control php conditions and mysqli connection database

--- db_connect/config.php
+++ db_connect/config.php
@@ -1,7 +1,12 @@
 <?php
 
-define('LOCALHOST', 'localhost');
-define('DBNAME', 'loginTest');
-define('DBID', 'root');
-define('DBMDP', '');
-?>
+$servername="localhost";
+$dBUsername= "root";
+$dBpassword = "";
+$dBName = "logintest";
+
+$db = mysqli_connect($servername, $dBUsername, $dBpassword, $dBName);
+
+if(!$db) {
+    die("connexion failed :".my_sqli_connect_error());
+}
--- views/access.inc.php
+++ views/access.inc.php
@@ -1,10 +1,11 @@
 <?php 
 
 if(isset($_POST['button-submit'])) {
-     require '../db_connect/connect.php';
+     require '../db_connect/config.php';
       
      $username = $_POST['email'];
      $password = $_POST['password'];
+
 
      if(empty($username) || empty($password)) {
           header("Location: ../login.php?error=emptyfields&email=".$username."&password=".$password);
@@ -30,11 +31,33 @@ if(isset($_POST['button-submit'])) {
             mysqli_stmt_store_result($stmt);
             $resultCheck = mysqli_stmt_num_rows($stmt);
             if($resultCheck > 0) {
-                header("Location: ../login.php?error=sqlerror");
+                header("Location: ../login.php?error=usertaken&email=".$username);
             exit();
+            }
+            else {
+                $sql = "INSERT INTO user (idUser, emailUser, mdpUser) VALUES (?, ?, ?)";
+                $stmt = mysqli_stmt_init($db);  
+                if (!mysqli_stmt_prepare($stmt, $sql)) {
+                    header("Location: ../login.php?error=sqlerror");
+                    exit();
+                }
+                else {
+                    $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
+
+                    mysqli_stmt_bind_param($stmt, "ss", $username, $password);
+                    mysqli_stmt_execute($stmt);
+                    header("Location: ../login.php?signup=success");
+                            exit();
+                }
             }
         }
      }
+        mysqli_stmt_close($stmt);
+        mysqli_close($db);
 
+                                                                }   
 
-}
+     else {
+         header("Location: ../login.php");
+         exit();
+     }
